1e561c25c6e7da055873dcfe4486c84a1fddd874
Samuel Bernard <samuel.bernard@s4m.io>
2015-10-07 20:45:43 -0400

    Set Centos version to 7.1 instead of 7

--- metadata.rb
+++ metadata.rb
@@ -2,13 +2,13 @@ name 'confluent-platform'
 maintainer 'Sam4Mobile'
 maintainer_email 'samuel.bernard@s4m.io'
 license 'Apache 2.0'
-description 'Installs/Configures confluent-platform'
+description 'Install/Configure confluent-platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://gitlab.com/s4m-chef-repositories/confluent-platform'
 issues_url 'https://gitlab.com/s4m-chef-repositories/confluent-platform/issues'
 version '1.0.0'
 
-supports 'centos',  '>= 7'
+supports 'centos',  '>= 7.1'
 
 depends 'cluster-search'
 depends 'yum'
