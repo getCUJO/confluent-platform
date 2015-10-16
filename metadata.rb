aed1b759f22d9391784a6d451fde768621f7d850
Samuel Bernard <samuel.bernard@s4m.io>
2015-10-16 18:34:28 -0400

    Start 1.1.0, update maintainer email

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name 'confluent-platform'
 maintainer 'Sam4Mobile'
-maintainer_email 'samuel.bernard@s4m.io'
+maintainer_email 'dps.team@s4m.io'
 license 'Apache 2.0'
 description 'Install/Configure confluent-platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://gitlab.com/s4m-chef-repositories/confluent-platform'
 issues_url 'https://gitlab.com/s4m-chef-repositories/confluent-platform/issues'
-version '1.0.1'
+version '1.1.0'
 
 supports 'centos',  '>= 7.1'
 
