73ae25ea9b3817534264b35e6fbebb726927a52e
Samuel Bernard <sb@make.org>
2017-04-25 18:37:40 +0200

    Fix metadata: license and chef_version

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,14 @@
 name 'confluent-platform'
 maintainer 'Make.org'
 maintainer_email 'sre@make.org'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Install/Configure confluent-platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://gitlab.com/chef-platform/confluent-platform'
 issues_url 'https://gitlab.com/chef-platform/confluent-platform/issues'
 version '2.2.0'
+
+chef_version '>= 12.0'
 
 supports 'centos', '>= 7.1'
 
