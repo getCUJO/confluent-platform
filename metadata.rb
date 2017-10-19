9822d3f91cfc50b16b89cfcefa43057925392874
Samuel Bernard <sb@make.org>
2017-10-19 17:33:44 +0200

    Start 2.6.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Install/Configure confluent-platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://gitlab.com/chef-platform/confluent-platform'
 issues_url 'https://gitlab.com/chef-platform/confluent-platform/issues'
-version '2.5.0'
+version '2.6.0'
 
 chef_version '>= 12.14'
 
